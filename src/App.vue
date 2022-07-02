da045cfa2d8a9f474e683986728636069f2bb83c
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-07-02 15:36:45 +0800

    Uninstall vue-router and update config vite

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <main>
-    <router-view></router-view>
+  <main class="mt-12">
+    <img class="mx-auto" alt="Vue logo" src="@asset/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite + Tailwind + Vue Router" />
   </main>
 </template>
+
+<script setup>
+
+import HelloWorld from '@component/HelloWorld.vue'
+
+</script>
